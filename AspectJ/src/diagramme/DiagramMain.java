810dc6b38e6137e74d1feb6908884ad5addb9228
Pansa Jose <pansa@b10p18.fil.univ-lille1.fr>
2016-09-22 17:22:56 +0200

    Digramme: mise en place des retours d'appels de méthodes

--- AspectJ/src/diagramme/DiagramMain.java
+++ AspectJ/src/diagramme/DiagramMain.java
@@ -23,32 +23,8 @@ public class DiagramMain {
 		orders.addOrder(order1);
 		orders.addOrder(order2);
 		
-		
-		/*
-		System.out.println("Main: #Clients: Est-ce que les références sont les mêmes ?");
-		System.out.println("Main: " + clients);
-		System.out.println("Main: " + clients2);
-		System.out.println("Main: #Clients: Est-ce que c'est le même objet ?");
-		System.out.println("Main: " + clients.getClient(0));
-		System.out.println("Main: " + clients2.getClient(0));
-		
-		System.out.println("Main: #Orders: Est-ce que les références sont les mêmes ?");
-		System.out.println("Main: " + orders);
-		System.out.println("Main: " + orders2);
-		System.out.println("Main: #Orders: Est-ce que c'est le même objet ?");
-		System.out.println("Main: " + orders.getOrders(0).getId());
-		System.out.println("Main: " + orders2.getOrders(0).getId());
-		*/
-		
-		//System.out.println("Main: #Hedi prend la commande order1 :");
 		hedi.addOrder(orders.getOrders(0));
 		
-		//System.out.println("Main: #Affichage de la commande order1 :");
-		//orders.getOrders(0).printOrder(); 
-		//System.out.println(" " + orders.getOrders(0).getClient());
-		
-		
-		//System.out.println("Main: #On essaye de supprimer Hedi");
 		try {
 			clients.delClient(hedi);
 		} catch(ClientHasOrderException e) {
@@ -57,15 +33,13 @@ public class DiagramMain {
 		
 		
 		hedi.delOrder(orders.getOrders(0));
-		//System.out.println("Main: #On supprime la dernière commande de Hedi");
+		
 		try {
 			clients.delClient(hedi);
 			//System.out.println("Main: La suppression de Hedi dans la liste de clients a été faite avec succès");
 		} catch(ClientHasOrderException e) {
 			//System.out.println("Main: L'exception ClientHasOrderException a été levée");
-		}
-		
-		
+		}		
 
 	}
 
